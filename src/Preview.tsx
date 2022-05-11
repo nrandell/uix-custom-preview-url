0c02b86a8fb919f9397a590c4283379ae998bf41
nrandell <nick@nickrandell.com>
2022-05-11 20:18:04 +0100

    Basic custom preview

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -5,37 +5,92 @@ import {
   useFormSidebarExtension,
   Wrapper,
 } from "@graphcms/uix-react-sdk";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 const declaration: FormSidebarExtensionDeclaration = {
   extensionType: ExtensionType.formSidebar,
   name: "Custom URL Preview",
   description: "Better cusomisation for preview urls",
-  config: {
-    WEBSITE_URL: {
-      type: ConfigType.string,
-      displayName: "Base URL",
-      description: "Base URL of the website to preview under",
-      required: true,
-    },
-  },
+  config: {},
   sidebarConfig: {
-    GENERAL: {
+    URL_TEMPLATE: {
       type: ConfigType.string,
-      displayName: "Basic config type",
-      description: "What value is this?",
-      required: false,
+      displayName: "The preview URL",
+      description: `The preview URL as a javascript interpolated string.`,
+      required: true,
     },
   },
 };
 
+const dynamicTemplate = (template: string, vars: Record<string, any>) => {
+  // eslint-disable-next-line no-new-func
+  const handler = new Function(
+    "vars",
+    "const tagged = (" +
+      Object.keys(vars).join(",") +
+      ") => `" +
+      template +
+      "`; return tagged(...Object.values(vars))"
+  );
+  return handler(vars);
+};
+
+function usePreviewUrl(template: string, values: Record<string, any>) {
+  const populatedUrl = useMemo(() => {
+    try {
+      // eslint-disable-next-line no-new-func
+      const result = dynamicTemplate(template, values);
+      return result;
+    } catch (error) {
+      console.log("error", error);
+      return (error as Error).message;
+    }
+  }, [template, values]);
+
+  return populatedUrl;
+}
+
 const Preview: React.FC = () => {
   const details = useFormSidebarExtension();
-  console.log("got details", details);
+  const {
+    extension: { sidebarConfig },
+    form: { subscribeToFormState, getState },
+  } = details;
+
+  const [values, setValues] = useState<Record<string, any>>(() => getState());
+
+  useEffect(() => {
+    let unsubscribe: (() => any) | undefined;
+
+    subscribeToFormState(
+      (sub) => {
+        setValues(sub.values);
+      },
+      {
+        active: true,
+        touched: true,
+        values: true,
+        initialValues: true,
+        submitSucceeded: true,
+      }
+    )
+      .then((u) => (unsubscribe = u))
+      .catch((error) => console.error("error in sub", error));
+
+    return () => {
+      unsubscribe?.();
+    };
+  }, [subscribeToFormState]);
+
+  const populatedUrl = usePreviewUrl(
+    sidebarConfig.URL_TEMPLATE as string,
+    values
+  );
+
   return (
-    <div>
-      <h2>Preview page</h2>
-    </div>
+    <a href={populatedUrl} target="_blank" rel="noreferrer">
+      Open Preview
+    </a>
   );
 };
 
@@ -46,7 +101,6 @@ export const MainPreview: React.FC = () => {
       undefined
     );
   }, []);
-  console.log("location", document.location, "uid", uid);
   return (
     <Wrapper declaration={declaration} uid={uid} debug={true}>
       <Preview />
